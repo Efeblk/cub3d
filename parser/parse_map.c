98a7aa7dc6b409b00f1e3de2b57e9faca7efed68
ismet efe balik <ibalik@k1m03s03.42istanbul.com.tr>
2023-12-14 18:20:38 +0300

    check line thing

--- parser/parse_map.c
+++ parser/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: ibalik <ibalik@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 17:37:25 by ibalik            #+#    #+#             */
-/*   Updated: 2023/12/14 17:50:55 by ibalik           ###   ########.fr       */
+/*   Updated: 2023/12/14 18:15:42 by ibalik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,51 @@ void parse_map(char *arg)
     while (get_next_line(fd, &line))
     {
         printf("%s\n", line);
-        while (*line == ' ' || *line == '\t')
-            line++;
-        
-        
+        check_line(line);        
         free(line);
     }
     close(fd);
+}
+
+void check_line(char *line)
+{
+    int i;
+
+    i = 0;
+    while (line[i])
+    {
+        while (line[i] == ' ' || line[i] == '\t')
+            i++;
+        if ((line[i] != 'W' && line[i + 1] != 'E') || 
+        (line[i] != 'E' && line[i + 1] != 'A') || 
+        (line[i] != 'S' && line[i + 1] != 'O') || 
+        (line[i] != 'N' || line[i + 1] != 'O') || 
+        line[i] != 'F' || 
+        line[i] != 'C')
+        {
+            error("Invalid character in map");
+        }
+        else
+        {
+            route_valid(&line[i]);
+        }
+        i++;
+    }
+}
+
+void route_valid(char *line)
+{ 
+    int i;
+    i = 0;
+    if ((line[i] != 'W' && line[i + 1] != 'E') || 
+        (line[i] != 'E' && line[i + 1] != 'A') || 
+        (line[i] != 'S' && line[i + 1] != 'O') || 
+        (line[i] != 'N' || line[i + 1] != 'O'))
+        {
+            check_xpm(&line[2]);
+        }
+        else if (line[i] != 'F' || line[i] != 'C')
+        {
+            check_rgb(&line[2]);
+        }
 }
---
+++ parser/parse_map_utils.c
@@ -0,0 +1,17 @@
+#include "cub3d.h"
+
+void check_xpm(char *line)
+{
+    int i;
+    i = 0;
+    while (line[i])
+    {
+        /* code */
+    }
+    
+}
+
+void check_rgb(char *line)
+{
+
+}
